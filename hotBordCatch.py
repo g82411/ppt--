b8a75e2003552540b7ecca3886642e52a5226885
= <lmz8241@nisra.net>
2014-06-05 20:54:13 +0800

    Function更新

--- hotBordCatch.py
+++ hotBordCatch.py
@@ -6,10 +6,19 @@ import requests
 import time
 from datetime import date
 URL='http://www.ptt.cc/hotboard.html'
-result=tuple(set(re.findall('/bbs/(\w+)/index.html',requests.get(URL).text.encode('utf-8','ignore'))))
 PATHs=('data','hotboard')
-for path in PATHs:
-    if not(os.path.exists(path) and os.path.isdir(path)):
-        os.mkdir(path)
-    os.chdir(path)
-pickle.dump(result,open('hotboard'+str(date.today())+'-'+str(time.localtime()[3])+'-'+str(time.localtime()[4])+'.pickle','wb'))
+def hotBordCatch():
+	result=tuple(set(re.findall('/bbs/(\w+)/index.html',requests.get(URL).text.encode('utf-8','ignore'))))
+	print result
+	goPath()
+	pickle.dump(result,open('hotboard'+str(date.today())+'-'+str(time.localtime()[3])+'-'+str(time.localtime()[4])+'.pickle','wb'))
+	pass
+def goPath():
+	for path in PATHs:
+	    if not(os.path.exists(path) and os.path.isdir(path)):
+	        os.mkdir(path)
+	    os.chdir(path)
+def catchPost(bordName):
+	html_src=requests.get('http://www.ptt.cc/bbs/'+bordName+'/index.html').text.encode('utf-8','ignore')
+	print html_src
+hotBordCatch()
